91cfd0598b6ae6471cedb69daa7dcf4492d6c862
lixianmin <lixianmin@live.cn>
2022-09-08 07:01:53 +0800

    现在jammo可以行走了哈

---
+++ timeline/Assets/Code/Client/Kits/PlayerMoveKit.cs
@@ -0,0 +1,25 @@
+
+/********************************************************************
+created:    2022-08-27
+author:     lixianmin
+
+Copyright (C) - All Rights Reserved
+*********************************************************************/
+
+using Unicorn;
+using UnityEngine;
+
+namespace Client
+{
+    public class PlayerMoveKit: KitBase
+    {
+        protected override void Update()
+        {
+            var transform = GetTransform();
+            const float moveSpeed = 2.0f;
+            var motion = transform.rotation * Vector3.forward * moveSpeed * Time.deltaTime;                
+            transform.Translate(motion, Space.World);
+            Console.WriteLine(motion);
+        }
+    }
+}
---
+++ timeline/Assets/Code/Unicorn/Kit/_KitFactory.cs
@@ -0,0 +1,21 @@
+
+// Warning: all code of this file are generated automatically, so do not modify it manually ~
+// Any questions are welcome, mailto:lixianmin@gmail.com
+
+using System;
+using System.Collections;
+
+namespace Unicorn
+{
+    public class _KitFactory
+    {
+        [UnityEngine.Scripting.Preserve]
+        private static Hashtable _GetLookupTableByName ()
+        {
+            return new Hashtable(1)
+            {
+                { "Client.PlayerMoveKit", (Func<KitBase>)(() => new Client.PlayerMoveKit()) },
+            };
+        }
+    }
+}
