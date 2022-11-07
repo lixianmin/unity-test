e45235b994d2aad1b591f77f9e23ebac83c55774
lixianmin <lixianmin@live.cn>
2022-11-07 17:03:18 +0800

    upgrade Unicorn

--- timeline/Assets/Code/Client/MbGame.cs
+++ timeline/Assets/Code/Client/MbGame.cs
@@ -17,12 +17,30 @@ namespace Client
         {
             // 避免Game对象在场景切换的时候被干掉
             GameObject.DontDestroyOnLoad(gameObject);
-            UnicornMain.Instance.Init();
+            _unicornMain.Init();
         }
 
         private void Update()
         {
-            UnicornMain.Instance.Update(Time.deltaTime);
+            // 成本帧
+            var deltaTime = Time.deltaTime;
+            _unicornMain.ExpensiveUpdate(deltaTime);
+
+            // 慢速帧
+            var time = Time.time;
+            if (time >= _nextSlowUpdateTime)
+            {
+                var slowDeltaTime = time - _lastSlowUpdateTime;
+                _lastSlowUpdateTime = _nextSlowUpdateTime;
+                _nextSlowUpdateTime = time + 0.1f;
+
+                _unicornMain.SlowUpdate(slowDeltaTime);
+            }
         }
+
+        private readonly UnicornMain _unicornMain = UnicornMain.Instance;
+
+        private float _lastSlowUpdateTime;
+		private float _nextSlowUpdateTime;
     }
 }
