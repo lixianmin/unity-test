0dda166e5287643a90e308225ea2dfc9184ed3e7
lixianmin <lixianmin@gmail.com>
2023-03-04 07:39:05 +0800

    refactor

--- solid-nft/src/App.jsx
+++ solid-nft/src/App.jsx
@@ -9,33 +9,37 @@ import {get_resource} from './code/tools/api';
 import Notification, {showNotification} from "./code/components/Notification";
 import {Button} from "solid-bootstrap";
 
-function init() {
-    get_resource().then((resp) => {
-        if (resp.code === 0) {
-            const resourceUrl = `https://${resp.data.bucket_url}/${resp.data.nft_pic_path}/`
-            localStorage.setItem("resourceUrl", resourceUrl);
-            console.log(resourceUrl)
-        } else {
-            showNotification('错误', resp.error)
-        }
-    });
+
+async function initResourceUrl() {
+    const resp = await get_resource()
+    if (resp.code === 0) {
+        const resourceUrl = `https://${resp.data.bucket_url}/${resp.data.nft_pic_path}/`
+        localStorage.setItem("resourceUrl", resourceUrl);
+        console.log(resourceUrl)
+    } else {
+        showNotification('错误', resp.error)
+    }
 }
 
-const App = () => {
-    init()
+async function init() {
+    try {
+        await initResourceUrl()
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+export default function App() {
+    init().then()
 
     function onClick() {
         showNotification("hello", "world")
     }
 
-    return (
-        <>
-            <Button onClick={onClick} variant="secondary">
-                show notification
-            </Button>
-            <Notification/>
-        </>
-    );
-};
-
-export default App;
+    return <>
+        <Button onClick={onClick} variant="secondary">
+            show notification
+        </Button>
+        <Notification/>
+    </>
+}
