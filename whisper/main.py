6d022ef4dfb0ab98979644c05d37a2aa45c0946d
lixianmin <lixianmin@gmail.com>
2023-03-18 14:46:48 +0800

    测试whisper

---
+++ whisper/main.py
@@ -0,0 +1,73 @@
+import wave
+
+import pyaudio
+import whisper
+
+# import numpy as np
+
+# Audio settings
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+CHUNK = 1024 << 4
+
+
+class AudioRecorder:
+    def __init__(self):
+        self._audio = pyaudio.PyAudio()
+        self._stream = self._audio.open(format=FORMAT, channels=CHANNELS, rate=RATE,
+                                        input=True, frames_per_buffer=CHUNK)
+
+    def close(self):
+        self._stream.stop_stream()
+        self._stream.close()
+        self._audio.terminate()
+
+    def take_record(self, seconds=30):
+        frames = []
+        for i in range(0, int(RATE / CHUNK * seconds)):
+            data = self._stream.read(CHUNK)
+            frames.append(data)
+
+        result = b''.join(frames)
+        return result
+
+    def save(self, filename, data):
+        with wave.open(filename, 'wb') as fout:
+            fout.setnchannels(CHANNELS)
+            fout.setsampwidth(self._audio.get_sample_size(FORMAT))
+            fout.setframerate(RATE)
+            fout.writeframes(data)
+
+
+def speech_to_text(filename):
+    # Transcribe speech to text
+    model = whisper.load_model('medium')
+    whisper_audio = whisper.load_audio(filename)
+    whisper_audio = whisper.pad_or_trim(whisper_audio)
+
+    mel = whisper.log_mel_spectrogram(whisper_audio).to(model.device)
+    _, probs = model.detect_language(mel)
+
+    # language = max(probs, key=probs.get)
+
+    options = whisper.DecodingOptions(fp16=False)
+    result = whisper.decode(model, mel, options)
+    text = result.text
+    return text
+
+
+# Loop to continuously record and transcribe speech
+index = 0
+while True:
+    recorder = AudioRecorder()
+    data = recorder.take_record()
+
+    index += 1
+    filename = f"output{index}.wav"
+    recorder.save(filename, data)
+    recorder.close()
+
+    text = speech_to_text(filename)
+
+    print(f"Text: {text}")
---
+++ whisper/record.py
@@ -0,0 +1,41 @@
+import pyaudio
+import wave
+
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+CHUNK = 1024
+RECORD_SECONDS = 5
+WAVE_OUTPUT_FILENAME = "output.wav"
+
+audio = pyaudio.PyAudio()
+
+# open microphone stream
+stream = audio.open(format=FORMAT, channels=CHANNELS,
+                    rate=RATE, input=True,
+                    frames_per_buffer=CHUNK)
+
+print("Recording...")
+
+frames = []
+
+# record audio data from microphone
+for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    data = stream.read(CHUNK)
+    frames.append(data)
+
+print("Finished recording.")
+
+# stop the stream and close the audio object
+stream.stop_stream()
+stream.close()
+audio.terminate()
+
+# save the audio data as a WAV file
+wave_file = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+wave_file.setnchannels(CHANNELS)
+wave_file.setsampwidth(audio.get_sample_size(FORMAT))
+wave_file.setframerate(RATE)
+wave_file.writeframes(b''.join(frames))
+wave_file.close()
+
